b3c34f0d694cf93ddb151dbe916d989a7135e17d
Sergio C. Orozco Torres <scot3004@gmail.com>
2016-04-08 18:01:03 -0500

    Animación hover al colocar el mouse encima del menú

--- resources/js/grayscale.js
+++ resources/js/grayscale.js
@@ -24,10 +24,6 @@ $(function() {
     });
 });
 
-// Closes the Responsive Menu on Menu Item Click
-$('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
-});
 /*
 // Google Maps Scripts
 // When the window has finished loading create our google map below
