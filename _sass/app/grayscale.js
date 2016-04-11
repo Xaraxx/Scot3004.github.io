df36f733ec51977f7bef9e887177e3a6d1d16754
Sergio C. Orozco Torres <scot3004@gmail.com>
2016-04-10 19:21:31 -0500

    Sidebar links
    Se estilizo el listado de enlaces ubicados en el sidebar

---
+++ _sass/app/grayscale.js
@@ -0,0 +1,22 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+$(window).scroll(function() {
+    if ($(".navbar-main").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
