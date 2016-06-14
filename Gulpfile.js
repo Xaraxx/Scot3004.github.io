80c9a4161eb069a2150ab5e07d0030cafe078e5b
Sergio C. Orozco Torres <scot3004@gmail.com>
2016-06-14 14:08:33 -0500

    Deleted tasks only left imagemin that still cannot be replaced

--- Gulpfile.js
+++ Gulpfile.js
@@ -4,67 +4,24 @@
 
 /* Get dependencies */
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
-    uglify = require('gulp-uglify'),
     imagemin = require('gulp-imagemin'),
-    rename = require('gulp-rename'),
-    concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    newer = require('gulp-newer'),
-    spawn = require('child_process').spawn;
+    newer = require('gulp-newer');
 
 /* Set paths */
 
 var paths = {
     /* Source paths */
-    styles: ['_sass/main.scss'],
-    scripts: [
-        'resources/js/**'
-    ],
-    vendor_scripts: [
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/jquery.easing/js/jquery.easing.js',
-        'node_modules/bootstrap/dist/js/bootstrap.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
-    ],
     images: ['resources/img/**'],
    
     /* Output paths */
-    stylesOutput: 'css',
-    scriptsOutput: 'js',
     imagesOutput: 'img'
 };
 
-/* Tasks */
-gulp.task('css', function() {
-    return sass(paths.styles,{ style: 'compressed' })
-        .pipe(rename({suffix: '.min'}))
-        .pipe(gulp.dest(paths.stylesOutput));
-});
-
-gulp.task('js', function() {
-    return gulp.src(paths.vendor_scripts.concat(paths.scripts))
-        .pipe(concat('main.js'))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(paths.scriptsOutput));
-});
 
 gulp.task('img', function() {
     return gulp.src(paths.images)
         .pipe(newer(paths.imagesOutput))
         .pipe(imagemin({ optimizationLevel: 5, progressive: true, interlaced: true }))
         .pipe(gulp.dest(paths.imagesOutput));
-});
-
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('_sass/**/*.scss',['css']);
-    gulp.watch(paths.images,['img']);
-    gulp.watch(paths.scripts,['js']);
-    gulp.watch(paths.fonts,['fonts']);
-});
-
-gulp.task('compile', function() {
-    gulp.start('css', 'js', 'img', 'fonts');
 });
