c5d3176a20c31ea88d8d779e442deeff352961bc
Sergio C. Orozco Torres <scot3004@gmail.com>
2016-07-24 22:14:20 -0500

    JavaScript fix

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,49 +1,49 @@
-var smoothScroll = require('jquery-smooth-scroll');
 global.$ = require('jquery');
 global.jQuery = global.$;
+var smoothScroll = require('jquery-smooth-scroll');
 var bootstrapjs = require('bootstrap');
 require('magnific-popup');
-
-function checktop(){
-  if($(".navbar-main").length){
-    if ($(".navbar-main").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+$(document).ready(function() {
+  function checktop(){
+    var navbar_main = $(".navbar-main");
+    if(navbar_main.length){
+      if (navbar_main.offset().top > 50) {
+          $(".navbar-fixed-top").addClass("top-nav-collapse");
+      } else {
+          $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      }
     }
   }
-}
 
-$(window).scroll(checktop);
+  $(window).scroll(checktop);
 
-$(document).ready(function() {
-	checktop();
-	
-	$('.popup-gallery').magnificPopup({
-		delegate: 'a',
-		type: 'image',
-		tLoading: 'Cargado imagen #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">La imagen #%curr%</a> no pudo ser cargada.',
-			titleSrc: function(item) {
-				return item.el.attr('title') + '<small>por Sergio Orozco</small>';
-			}
-		}
-	});
-});
+  checktop();
+  
+  $('.popup-gallery').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Cargado imagen #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">La imagen #%curr%</a> no pudo ser cargada.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>por Sergio Orozco</small>';
+      }
+    }
+  });
 
-$('a.page-scroll').smoothScroll({
-  beforeScroll: function(options){
-    $(".navbar-collapse").removeClass('in');
-  },
-  afterScroll: function(options) {
-    $(this).blur();
-  },
-  easing: 'swing'
+  $('a.page-scroll').smoothScroll({
+    beforeScroll: function(options){
+      $(".navbar-collapse").removeClass('in');
+    },
+    afterScroll: function(options) {
+      $(this).blur();
+    },
+    easing: 'swing'
+  });
 });
